98c120f3187e31568226fbdff5a6e5d8a83539c8
fangqiming <847271387@qq.com>
2019-06-15 20:08:31 +0800

    k

--- src/apiConfig/api.js
+++ src/apiConfig/api.js
@@ -1,8 +1,8 @@
-let localhostDev =false;
+let localhostDev =true;
 //如果是本地开发，就把localhostDev 设置成true，连接就是拼成‘http://localhost:8080/api/test’
 //生产环境，就把localhostDev 设置成false，连接就是拼成‘http://xxx.com/api/test’
 //let _host = localhostDev ? 'http://fqm0925.vicp.io:11336' : 'http://www.1000stock.com';
-let _host = localhostDev ? 'http://192.168.2.165:8080': 'http://www.1000stock.com';
+let _host = localhostDev ? 'http://127.0.0.1:8080': 'http://www.1000stock.com';
 
 let httpUrl = {
   'newSearchLastGainApi': _host + '/trade/find_gain',//首页0331修改后获取最新赢利数据接口
